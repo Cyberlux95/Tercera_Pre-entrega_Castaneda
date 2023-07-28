1c4c6f8ac6345e2d8bc848831ebcd969c12cf845
Luis Castañeda <luis.ecs@hotmail.com>
2023-07-28 00:45:19 -0600

    agregar super user y modelos

--- complejo_habitacional/habitaciones/admin.py
+++ complejo_habitacional/habitaciones/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Residente, Vehiculos, Visitantes
 
 # Register your models here.
+admin.site.register(Residente)
+admin.site.register(Vehiculos) 
+admin.site.register(Visitantes)
